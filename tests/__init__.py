0f8176511a4ca2f1ebe4b98d879f7f1b78a97bc0
Prakash Pandey <prakash.pandey@openlabs.co.in>
2014-11-24 18:38:09 +0530

    Migrated to 3.2

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,5 +6,4 @@
     :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
-#Flake8: noqa
-from test_nereid_review import suite
+from test_nereid_review import suite  # noqa
